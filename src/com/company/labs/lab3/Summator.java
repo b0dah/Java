c029f31eb27644f291e8e5bebf682ea08e3bf902
b0dah <ries8898@gmail.com>
2019-09-26 09:18:03 +0300

    included unit tests

---
+++ src/com/company/labs/lab3/Summator.java
@@ -0,0 +1,14 @@
+package com.company.labs.lab3;
+
+public class Summator {
+
+    int result;
+
+    public static int summFunction(int a, int b) {
+        return a + b;
+    }
+
+    public static int multiplicationFunction(int a, int b) {
+        return a * b;
+    }
+}
---
+++ src/test/lab3/SummatorTest.java
@@ -0,0 +1,17 @@
+package test.lab3;
+
+import com.company.labs.lab3.Summator;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SummatorTest {
+    @Test
+    public void summFunctionTest() {
+        Assertions.assertEquals(4, Summator.summFunction(2,2));
+    }
+
+    @Test
+    public void multiplicationFunctionTest() {
+        Assertions.assertEquals(4, Summator.multiplicationFunction(2,2));
+    }
+}
