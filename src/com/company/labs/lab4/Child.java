e743c6b02eb5af4db88acf71ef1561e19f7fe486
b0dah <ries8898@gmail.com>
2019-10-17 08:42:04 +0300

    lab4 added

--- src/com/company/labs/lab4/Child.java
+++ src/com/company/labs/lab4/Child.java
@@ -1,6 +1,13 @@
 package com.company.labs.lab4;
 
 public class Child extends Parent implements Fly {
+    @Override
+    public void fly() {
+        System.out.println("let's fly");
+    }
+
+    @Override
+    public void f() {
 
     }
 
---
+++ src/com/company/labs/lab4/UniversityDemo.java
@@ -0,0 +1,20 @@
+package com.company.labs.lab4;
+
+class UniversityMember {
+
+}
+
+
+
+
+
+
+
+
+public class UniversityDemo {
+    public static void main(String[] args) {
+
+    }
+
+
+}
