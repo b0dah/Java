e4528e97a550e29e8690f55988308908a89897da
b0dah <ries8898@gmail.com>
2019-11-28 00:36:11 +0300

    serialized own model

--- src/com/company/labs/lab6/SerializationDemo.java
+++ src/com/company/labs/lab6/SerializationDemo.java
@@ -34,13 +34,13 @@ public class SerializationDemo {
         }
     }
 
-    private static UserModel deserializeObject() {
-        UserModel instance = null;
+    private static OrderDataModel deserializeObject() {
+        OrderDataModel instance = null;
         try {
             FileInputStream file = new FileInputStream(filename);
             ObjectInputStream objectInputStream = new ObjectInputStream(file);
 
-            instance = (UserModel) objectInputStream.readObject();
+            instance = (OrderDataModel) objectInputStream.readObject();
 
             objectInputStream.close();
             file.close();
@@ -54,14 +54,35 @@ public class SerializationDemo {
         return instance;
     }
 
+    private static void prettyModelPrint(OrderDataModel order) {
+        System.out.println("\n === Order ===\n" +
+                order.getCustomerName() + "\n" +
+                order.getDeliveryTime() + "\n" +
+                order.getOriginAdress() + "\n" +
+                order.getDestinantionAddress() + "\n" +
+                order.getNumberOfMovers() + "\n" +
+                order.getPayment() + "\n" +
+                order.getPhoneNumber() + "\n" +
+                order.getStatus() + "\n" );
+    }
+
     public static void main(String[] args) {
-        UserModel user = new UserModel("name", "e-mail");
+        OrderDataModel order = new OrderDataModel();
+        order.setId(100);
+        order.setCustomerName("Байер (странное имя)");
+        order.setDeliveryTime("08:00");
+        order.setOriginAdress("Park Lane, 78");
+        order.setDestinantionAddress("St. James, 21");
+        order.setNumberOfMovers(5);
+        order.setPayment(1000);
+        order.setPhoneNumber("8-800-000-00-88");
+        order.setStatus(0);
 
-        serializeObject(user);
-        UserModel deserializeInstance = deserializeObject();
 
-        System.out.println("Deserialized Object " + deserializeInstance.name);
+        serializeObject(order);
+        OrderDataModel deserializeInstance = deserializeObject();
 
-
+        System.out.println("\n DESERIALIZED OBJECT: ");
+        prettyModelPrint(deserializeInstance);
     }
 }
